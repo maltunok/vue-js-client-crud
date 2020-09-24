7e971e9963c9678af9cbe27e41ef61c0a5991e61
mustafa <m_altunok@hotmail.con>
2020-09-24 17:00:51 +0300

    version 2

--- src/components/AddTutorial.vue
+++ src/components/AddTutorial.vue
@@ -75,19 +75,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.submit-form{
+  max-width:300px;
+  margin:auto;
 }
 </style>
